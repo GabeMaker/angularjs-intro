3fcba7eca11536c9de0b30469fc157180ab41e5e
gabe <GabeMaker@users.noreply.github.com>
2015-03-18 22:35:07 +0000

    end of day status

--- js/README.md
+++ js/README.md
@@ -1,3 +1,11 @@
 What scope is the variable githubUserSearch in? Refresh index.html and use your console to find out. Do you think this is the right place for this to be?
 
 karma init with Louise
+
+- question 2 how to label what was searched for
+
+- adding access token to key value pair
+
+- iife global scope
+
+angular directives - can create own
--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -3,14 +3,22 @@ githubUserSearch.controller('GitUserSearchController', function($scope, $resourc
   var searchResource = $resource('https://api.github.com/search/users');
 
   $scope.doSearch = function() {
-    $scope.searchResult = searchResource.get(
-      { q: $scope.searchTerm }
-    );
+    if ($scope.searchTerm.length > 0) {
+      $scope.searchResult = searchResource.get({
+        q: $scope.searchTerm
+      });
+      console.log($scope.searchTerm)
+    } else {
+      // remove stuff from index
+    }
   };
 
   $scope.clearSearch = function() {
     $scope.searchTerm = ""
   };
+
+// var key = "54abf14be2425c478fb845387cc644acb3d53967";
+
+54abf14be2425c478fb845387cc644acb3d53967
 });
 
-'https://api.github.com/search/users?q='
