02909eb23c8ced9dda4270375dd145bf74c99ee3
gabe <GabeMaker@users.noreply.github.com>
2015-03-23 12:52:45 +0000

    getting ready for heroku

--- public/js/gitUserSearchController.js
+++ public/js/gitUserSearchController.js
@@ -1,8 +1,22 @@
-gitHubUserSearch.controller('GitUserSearchController', function($scope, $resource) {
+gitHubUserSearch.controller('GitUserSearchController', function($scope, $resource, $http) {
 
   var searchResource = $resource('https://api.github.com/search/users');
 
-  $scope.doSearch = function(){
-    $scope.searchResult = searchResource.get( { q: $scope.searchTerm});
+  // $scope.doSearch = function(){
+  //   $scope.searchResult = searchResource.get({q: $scope.searchTerm});
+  // };
+
+  $scope.doSearch = function() {
+    $scope.searchResult = searchResource.get({
+      q: $scope.searchTerm,
+    }, function(){
+      var username = 'https://api.github.com/users/' + $scope.searchTerm;
+        $http.get(username).
+          success(function(data){
+            $scope.username = data
+          });
+    });
+
   };
+
 });
