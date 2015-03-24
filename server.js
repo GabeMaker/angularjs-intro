3a5716aae6e54482aab7fd5fccfdfaab651f66fe
gabe <GabeMaker@users.noreply.github.com>
2015-03-24 08:58:55 +0000

    replace hardcoded port with env variable with default

--- server.js
+++ server.js
@@ -8,8 +8,36 @@ app.get('/', function(request, response) {
   response.render('index');
 });
 
-server.listen(3000, function() {
-  console.log("Server listening on port 3000");
+server.listen(process.env.PORT || 3000, function() {
+  console.log("Server listening on *");
 });
 
 module.exports = server;
+
+
+
+
+
+// // SO Question
+
+// http.listen(3000, function(){
+//  console.log('listening on *:3000');
+// });
+
+// // SO Answer
+
+// http.listen(process.env.PORT || 3000, function(){
+//   console.log('listening on', http.address().port);
+// });
+
+// // Original Implementation
+
+// server.listen(3000, function() {
+//   console.log("Server listening on port 3000");
+// });
+
+// // Trying Implementation
+
+// server.listen(process.env.PORT || 3000, function() {
+//   console.log("Server listening on *");
+// });
